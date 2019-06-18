2f2755d11bb29d9f69c07281b97ba733821fce67
tiagomoi <47538122+tiagomoi@users.noreply.github.com>
2019-06-18 20:06:59 -0300

    feat: add search term prefix

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 const readline = require('readline-sync')
+const robots = {
+  userInput: require('./robots/user-input.js'),
+	text: require('./robots/text.js')
+
+}
 
  function start() {
    const content = {}
-  
-   content.searchTerm = askAndReturnSearchTerm()
-   content.prefix = askAndReturnPrefix()
 
-   function askAndReturnSearchTerm() {
-     return readline.question('Type a wikipedia search term: ')
-  }
-
-   function askAndReturnPrefix() {
-     const prefixes = ['Who is', 'What is','The history of']
-     const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-     const selectedPrefixText = prefixes[selectedPrefixIndex]
-    
-     return selectedPrefixText
-   }
-
+   robots.userInput(content)
+   robots.text(content)
+   
   console.log(content)
 }
 start()
